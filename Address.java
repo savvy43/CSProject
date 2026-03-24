ab49787b064948606bc9f3b3fde5e56ce6797f8b
250187 <250187@newuu.uz>
2026-03-24 13:41:52 +0500

    Address datatype

---
+++ Address.java
@@ -0,0 +1,14 @@
+public class Address {
+    String streetAddress;
+    String city;
+    String state;
+    String zipcode;
+    String country;
+    public Address(String streetAddress, String city, String state, String zipcode, String country) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.country = country;
+    }
+}
